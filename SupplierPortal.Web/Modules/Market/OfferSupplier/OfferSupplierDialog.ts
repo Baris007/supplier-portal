46b16331480a44489b31f8ff896f1c469d2de038
Zeki_Efe <alperen@dividesoft.com>
2024-08-20 10:42:59 +0300

    gitignore edited

--- SupplierPortal.Web/Modules/Market/OfferSupplier/OfferSupplierDialog.ts
+++ SupplierPortal.Web/Modules/Market/OfferSupplier/OfferSupplierDialog.ts
@@ -1,11 +1,12 @@
-﻿import { OfferSupplierForm, OfferSupplierRow, OfferSupplierService } from '@/ServerTypes/Market';
-import { Decorators, EntityDialog } from '@serenity-is/corelib';
+import { OfferSupplierForm, OfferSupplierRow, OfferSupplierService } from '@/ServerTypes/Market';
+import { Decorators } from '@serenity-is/corelib';
+import { GridEditorDialog } from '@serenity-is/extensions';
 
 @Decorators.registerClass('SupplierPortal.Market.OfferSupplierDialog')
-export class OfferSupplierDialog extends EntityDialog<OfferSupplierRow, any> {
+export class OfferSupplierDialog extends GridEditorDialog<OfferSupplierRow> {
     protected getFormKey() { return OfferSupplierForm.formKey; }
     protected getRowDefinition() { return OfferSupplierRow; }
-    protected getService() { return OfferSupplierService.baseUrl; }
+    //protected getService() { return OfferSupplierService.baseUrl; }
 
     protected form = new OfferSupplierForm(this.idPrefix);
 }
